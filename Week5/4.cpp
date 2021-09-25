64a9b817acb9673f5ab58df06dcd11749c0833c9
ndhieu2005 <87236080+ndhieu2005@users.noreply.github.com>
2021-09-26 01:25:04 +0700

    Create 4.cpp

---
+++ Week5/4.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+/*Coi x là thành phố 1
+ y là thành phố 2
+ z là khoảng cách
+ (x;y;z)
+*/
+struct findway{
+    int x;
+    int y;
+    int z;
+};
+
+void map(vector<findway> map){
+    map.resize(5);
+    cout<<"Custom bản đồ: "<<endl;
+    for(int i=0;i<5;i++){
+        cin>>map[i].x>>map[i].y>>map[i].z;
+    }
+}
+void neighbor(vector<findway> map){
+    cout <<"Các thành phố lân cận: "<<endl;
+    for(int i =0;i<5;i++){
+        for(int j=1;j<=5;j++){
+            if(map[i].x==j)
+                cout<<map[i].x<<" and "<<map[j].y<<endl;
+            if(map[i].y==j)
+                cout<<map[i].y<<" and "<<map[i].x<<endl;
+        }
+    }
+}
+void distance(vector<findway> map){
+    int n;
+    cout<<"U wanna find the way of: ";
+    cin>>n;
+    for(int i=0;i<5;i++){
+        if(map[i].x==k)
+            cout<<"Distance from "<<map[i].x<<" to "<<map[i].y<<":"<<map[i].z<<endl;
+        if(map[i].y==k)
+            cout<<"Distance from"<<map[i].y<<" to "<<map[i].z<<":"<< map[i].z<<endl;
+    }
+}
+int main(){
+    vector<findway> way;
+    map(way);
+    neighbor(way);
+    distance(way);
+    return 0;
+}
