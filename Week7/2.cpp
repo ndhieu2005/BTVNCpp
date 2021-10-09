85ea0602e5be15b45ca54ce357996692d67c4a1b
ndhieu2005 <87236080+ndhieu2005@users.noreply.github.com>
2021-10-09 21:30:26 +0700

    Create 2.cpp

---
+++ Week7/2.cpp
@@ -0,0 +1,29 @@
+#include <string.h>
+#include <iostream>
+#include <map>
+using namespace std;
+map<char,int> m;
+map<char,int> :: iterator it;
+int main(){
+    string str="";
+    cin>>str;
+    //Chuyển các chữ thành chữ in hoa để thao tác
+    for(int i=0;i<str.size();i++){
+        if(str[i] >= 97 && str[i] <= 122){
+            str[i] -= 32;
+        }
+    }//Vứt các phần tử vào map để đếm
+    for(int i=0;i<str.size();i++){
+        m[str[i]]++;
+    }
+    //Đếm key của map, nếu = 26 thì đúng
+    int count =0;
+    for(it = m.begin();it != m.end();it++ ){
+        count++;
+    }
+    if(count == 26)
+        cout<<"Yes";
+    else
+        cout<<"No";
+    return 0;
+}
